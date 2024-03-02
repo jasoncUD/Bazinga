5159511abfaed49d91f21171cb683eb75d609779
jasoncUD <jasonc@udel.edu>
2024-03-02 14:30:20 -0500

    Made css to navbar

--- bazinga-web/src/App.tsx
+++ bazinga-web/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import { Outlet } from 'react-router';
 import Navbar from './components/Navbar';
+import AuthSwitchComponent from './components/AuthSwitchComponent';
 
 function App() {
   return (
     <div className="App">
           <Navbar/>
           <Outlet />
-      <h1>Bazinga</h1>
     </div>
   );
 }
