e7e5f0f560523c102c5012ddb7a9e0311226fadf
Rohan Nambiar <hellorohan.nambiar@gmail.com>
2024-03-02 12:35:37 -0500

    Created front end

---
+++ bazinga-app/src/main/java/com/henhacks/bazingaapp/EnvLoader.java
@@ -0,0 +1,26 @@
+package com.henhacks.bazingaapp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class EnvLoader {
+
+    public static void loadDotenv() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("workout-app-project\\.env"));
+            for (String line : lines) {
+                String[] parts = line.split("=", 2);
+                if (parts.length == 2) {
+                    String key = parts[0];
+                    String value = parts[1];
+                    System.setProperty(key, value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
