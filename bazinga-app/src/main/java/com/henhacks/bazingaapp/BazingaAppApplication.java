552dec3504a9dad5ccaef7dec96f447e6fc16d8d
Rohan Nambiar <hellorohan.nambiar@gmail.com>
2024-03-02 12:29:06 -0500

    Created backend

---
+++ bazinga-app/src/main/java/com/henhacks/bazingaapp/BazingaAppApplication.java
@@ -0,0 +1,13 @@
+package com.henhacks.bazingaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazingaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazingaAppApplication.class, args);
+	}
+
+}
---
+++ bazinga-app/src/test/java/com/henhacks/bazingaapp/BazingaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henhacks.bazingaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazingaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
