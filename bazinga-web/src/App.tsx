e5d4f01b93aaea020d2e2b05eb7e8449b3b36f22
TAYLOR KADANS <tkadans@udel.edu>
2024-03-02 14:02:49 -0500

    sent

--- bazinga-web/src/App.tsx
+++ bazinga-web/src/App.tsx
@@ -5,8 +5,21 @@ import { Instructor } from './Instructor';
 
 function App() {
   return (
-    <div>
-      <Instructor />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
