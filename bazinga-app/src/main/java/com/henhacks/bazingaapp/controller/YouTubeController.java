758634382d0e3bacf2b0e752eba5df310f1e6af3
Rohan Nambiar <hellorohan.nambiar@gmail.com>
2024-03-03 07:24:55 -0500

    added youtube backend

---
+++ bazinga-app/src/main/java/com/henhacks/bazingaapp/controller/YouTubeController.java
@@ -0,0 +1,34 @@
+package com.henhacks.bazingaapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.henhacks.bazingaapp.service.YouTubeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/youtube")
+@CrossOrigin(origins = {"http://localhost:3000"})
+public class YouTubeController {
+
+    @Autowired
+    private YouTubeService youTubeService;
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchVideos(@RequestBody String query) {
+        try {
+            List<String> videoUrls = youTubeService.searchVideos(query);
+            return ResponseEntity.ok(videoUrls);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Failed to search videos: " + e.getMessage());
+        }
+    }
+}
