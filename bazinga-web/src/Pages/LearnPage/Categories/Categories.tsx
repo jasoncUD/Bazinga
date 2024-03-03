00a60823e2b07e67343284db2b42f4bba2e3cbde
jasoncUD <jasonc@udel.edu>
2024-03-03 03:40:11 -0500

    add

--- bazinga-web/src/Pages/LearnPage/Categories/Categories.tsx
+++ bazinga-web/src/Pages/LearnPage/Categories/Categories.tsx
@@ -1,30 +1,29 @@
 import React, { FC, useState } from "react";
-import './Categories.css'; // Assume you have some basic styles defined
+import "./Categories.css"; // Assume you have some basic styles defined
 import Playing from "../Playing/Playing";
 import { Category } from "../../../interfaces/category";
+import { Question } from "../../../interfaces/question";
 
 interface CategoriesProps {
-    categoryList: Category[];
+  categoryList: Category[];
 }
 
 const Categories: React.FC<CategoriesProps> = (props) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const promptPlaying = (questions: Question[]) => {
+
+  };
   return (
     <div className="categories-container">
       <h1>Categories</h1>
       <div className="category-list">
         {props.categoryList.map((category) => (
-          <div
-            key={category.name}
-            className="category-item"
-          >
-            {category.name}
+          <div key={category.name} className="category-item">
+            <button> onClick={() => promptPlaying(category.questions)} {category.name}</button>
           </div>
         ))}
       </div>
-      {isPlaying && (
-        <Playing />
-      )}
+      {isPlaying && <Playing />}
     </div>
   );
 };
