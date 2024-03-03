9b534d89a11b20f91eddc59c5a716f2fda90dac5
jasoncUD <jasonc@udel.edu>
2024-03-02 21:08:35 -0500

    Adding to playing

--- bazinga-web/src/Pages/LearnPage/Playing/Playing.tsx
+++ bazinga-web/src/Pages/LearnPage/Playing/Playing.tsx
@@ -8,12 +8,21 @@ interface PlayingProps {
 }
 
 function Playing(props: PlayingProps) {
-  const [sampleQuestion, setSampleQuestion] = useState<Question>();
-
-
+  const [sampleQuestion, setSampleQuestion] = useState<Question>({
+    question: "What is the capital of France?",
+    option1: "Paris",
+    option2: "London",
+    option3: "New York",
+    optionCorrect: "Paris",
+  });
   return (
     <div>
         <h1>Get the prompt for {props.task} and {props.subject} </h1> 
+        <h2>{sampleQuestion.question}</h2>
+        <button>{sampleQuestion.option1}</button>
+        <button>{sampleQuestion.option2}</button>
+        <button>{sampleQuestion.option3}</button>
+        
 
     </div>
   );
