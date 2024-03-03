bc0e38b7d229dbb903d095577cc9fe152af39db2
Rohan Nambiar <hellorohan.nambiar@gmail.com>
2024-03-03 10:45:46 -0500

    sped up loading

--- bazinga-app/src/main/java/com/henhacks/bazingaapp/service/OpenAIService.java
+++ bazinga-app/src/main/java/com/henhacks/bazingaapp/service/OpenAIService.java
@@ -28,7 +28,7 @@ public class OpenAIService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
-        String content = "Provide all this in JSON format. Make a syllabus for a student, who wants to learn the subject: " + apiPrompt.getSubject() + " for grade level: " + apiPrompt.getGradeLevel() + ". Return 5 topics (topics) based on the grade level and subject. We need 5 questions per topic based on the subject and grade level (questions). Make this a multiple choice questions-quiz with 4 options per each question (options). Additionally, attach the explanation for the answer (explanation).";
+        String content = "Provide all this in JSON format. Make a syllabus for a student, who wants to learn the subject: " + apiPrompt.getSubject() + " for grade level: " + apiPrompt.getGradeLevel() + ". Return 5 topics (topics) based on the grade level and subject. We need 3 questions per topic based on the subject and grade level (questions). Make this a multiple choice questions-quiz with 4 options per each question (options). Additionally, attach the explanation for the answer (explanation).";
 
         // Create the request body
         String requestBody = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"" + content + "\"}], \"response_format\":{\"type\": \"json_object\"} }";
